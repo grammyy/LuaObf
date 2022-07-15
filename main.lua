bf98df36e8f0b0969b06221ba2ada820f7e14526
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-15 16:30:21 -0400

    -help

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 _G.switch = function(param, case_table)
     local case = case_table[param]
-    if case then return case() end
-end
-print(tostring(arg))
-print(arg[1])
+    if case then return case() end end
 for i,a in pairs(arg) do
-    
-    switch(a,{["bytes"]=function()print("ran") end})
+    switch(a,{["-help"]=function()
+        print("-byte     |-b |  Converts scripts to their binary codes, separated -split next argument")
+        print("-split    |-s |  Configures the seperating character for -b")
+        print("-range    |-r |  Configures the minimal byte code for -b")
+        print("-replace  |-r |  Replaces the default function for -b")
+        print("-graphic  |-gc|  Sets the Obfuscator into graphical mode.")
+        end})
 end
 --print((io.open(arg[1],"rb"):read "*a"):gsub(".",function(b)return"\\"..b:byte()end))
 --print(io.open(arg[1],"rb"):read "*a")
