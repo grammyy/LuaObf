4561c961d16b51f548f437640431f8da369cc7bf
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-15 21:47:42 -0400

    bytecode arguments

--- main.lua
+++ main.lua
@@ -1,14 +1,17 @@
+local app={
+    split="/"}
 _G.switch = function(param, case_table)
     local case = case_table[param]
     if case then return case() end end
 for i,a in pairs(arg) do
-    print(i)
     switch(a,{
         ["-byte"]=function()
             if arg[i+1]=="-run" then
                 else
-                print((io.open(arg[i-1],"rb"):read "*a"):gsub(".",function(b)return"\\"..b:byte()end))
+                print((io.open(arg[i-1],"rb"):read "*a"):gsub(".",function(b)return app.split..b:byte()end))
             end end,
+        ["-split"]=function()
+            app.split=arg[i+1]end,
         ["-help"]=function()
             print("-byte     |-b |  Converts scripts to their binary codes, separated -split next argument")
             print("-split    |-s |  Configures the seperating character for -b")
