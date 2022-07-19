a17090e36eb686d72f7e94eddfe297537d619ed0
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-19 00:02:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,31 +1,29 @@
 local app={
     split="/",
-    folder=io.popen("cd"):read().."\\output\\"}
+    path=io.popen("cd"):read(),
+    output=""}
 _G.switch=function(p,t)
-    local case=t[p]
-    if case then return case()end end
-_G.bytecode=function(f,d)
-    local r=(io.open(d,"rb"):read "*a"):gsub(".",function(b)return app.split..b:byte()end)
-    io.open(f,"w+"):write(r) print(r)end
-for i,a in pairs(arg) do --Add argument checks
+    if t[p] then return t[p]()
+    else if t["default"]then return t["default"]()end end end
+_G.bytecode=function(f)
+    local r
+    if switch("-run",arg)then
+    else r=(io.open(app.path.."\\"..f,"rb"):read "*a"):gsub(".",function(b)return app.split..b:byte()end)end
+    io.open(app.output.."\\"..f,"w+"):write(r) print(r)end
+_G.class=function(a)
+    local p=string.gmatch(a, '([^,]+)')
+    for g in p do
+        print(g) end end
+for i,a in pairs(arg) do
     switch(a,{
         ["-byte"]=function()
-            local d=arg[i-1]:gmatch(",")
-            if type(d)=="string" then
-                if io.type(io.open(d))=="file" then
-                    bytecode(app.folder..d,d)
-                else
-                    for _i,f in io.popen('find "'..d..'" -type f') do
-                        if switch("-cls") then os.execute("cls") end --Not compatible with linux
-                        bytecode(app.folder..f,d)end end
-            else
-                for _i,f in arg[i-1] do
-                    if switch("-cls") then os.execute("cls") end --Not compatible with linux
-                    bytecode(app.folder..f,d)end end
-            end,
-            --if switch("-run",arg) then end
+            if app.output~="" then
+            for f in string.gmatch(arg[i-1],'([^,]+)')do
+                bytecode(f)end
+            else print("ERR 001.  Output file not defined; -out usage required")end end,
+            ----if switch("-run",arg) then end
         ["-split"]=function()
-            if not switch("-long",arg) then
+            if not switch("-long",arg)then
                 app.split=arg[i+1]end end,
         ["-long"]=function()
             app.split=","end, --Add long encasing later
@@ -33,6 +31,10 @@ for i,a in pairs(arg) do --Add argument checks
             os.execute("cls")end, --not compatible with linux
         ["-folder"]=function()
             app.folder=arg[i+1]end, --not compatible with linux
+        ["-out"]=function()
+            app.output=arg[i+1]end,
+        ["-min"]=function()
+            print(io.open(arg[i-1],"r"):read("*a"):gsub("%-%-[^\n\r]+", ""):gsub("%-%-",""):gsub("\n",""):gsub("%s+"," "))end, --extremely simple minifier, just enough to actually work <3
         ["-help"]=function() --Seperate into categories and function later
             print("-byte     |-b |  Converts scripts to their binary codes, separated -split next argument")
             print("-long     |-l |  Overrides -split for -byte, best for performance when running obfuscated files")
@@ -43,11 +45,9 @@ for i,a in pairs(arg) do --Add argument checks
             print("-popout   |-p |  Opens the output in a seperate EXE process, doesn't affect general process.")
             print("-run      |-r |  Runs a obfuscated file, supported on -byte.")
             print("-min      |-m |  Compresses the target files into their simplest form.")
-            print("-keep     |-k |  Modifys past argument to keep certain elements of the file, such as variables.")
-            print("-cls      |-c |  Clears the screen")
+            print("-keep     |-k |  Modifys arguments to keep certain elements of the file, such as variables.")
+            print("-out      |-o |  Defines the output folder for compiled scripts.")
           --print("-dog      |-d |  dog") dog
-            print("-folder   |-f |  Defines folder. By default, uses the local output folder and output mode.")
-            print("-multi    |-mm|  Specifies multiple files to process, can be assigned to entire folder, multiple folders, or multiple files.")
-            end})end
---print((io.open(arg[1],"rb"):read "*a"):gsub(".",function(b)return"\\"..b:byte()end))
---print(io.open(arg[1],"rb"):read "*a")
+            end})end --print("ERR 001.       Output file not defined; -out usage required")
+--pint((io.open(arg[1],"rb"):read "*a"):gsub(".",function(b)return"\\"..b:byte()end))
+--pint(io.open(arg[1],"rb"):read "*a")
